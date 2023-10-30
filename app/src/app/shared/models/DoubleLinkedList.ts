b4df42195c636cc72349df6691dcf9a025db6187
Cody Fleenor <35227631+jack-fleenor@users.noreply.github.com>
2023-10-30 15:31:37 -0700

    TODO:V finish double linked list

--- app/src/app/shared/models/DoubleLinkedList.ts
+++ app/src/app/shared/models/DoubleLinkedList.ts
@@ -5,82 +5,23 @@ export class DoubleLinkedList {
   private _count : number = 0;
   public get count() : number { return this._count; };
   
-  public remove(_id : string)
+  public set push(value : number)
   {
-    let current = this.head;
-    while(current)
+    if(!this.head)
+    { this.head = new DoublePointedNode(value); }
+    else
     {
-      if(current.id === _id) {
-        this.head = current.next; 
-        --this._count;
-      }
-      if(current.next === null) { return; }
-      if(current.next.id === _id)
-      {
-        if(current.next.next !== null) { current.next = current.next.next; }
-        else { current.next = null; }
-        --this._count;
-      }
-      current = current.next;
+      const temp = new DoublePointedNode(value);
+      temp.next = this.head;
+      this.head.prev = temp;
+      this.head = temp;
     }
-    --this._count;
   }
-  public insertFront(data : number) : DoublePointedNode
-  {
-    const node = new DoublePointedNode(data);
-    if(!this.head) this.head = node;
-    else {
-      node.next = this.head;
-      this.head = node;
-    }
-    ++this._count;
-    return node;
-  }
-  public insertAt(value : number, target : string, direction: string) : DoublePointedNode | null
-  {
-    const node = new DoublePointedNode(value);
-    let current = this.head;
-    while(current)
-    {
-      if(current.id === target)
-      {
-        if(direction === 'back')
-        {
-          node.next = current;
-          current = node;
-        }
-        else if(direction === 'front')
-        {
-          if(current.next !== null) { node.next = current.next; }
-          current.next = node;
-        }
-        return node;
-      }
-      else
-      { current = current.next; }
-    }
-    return null;
-  }
-  public insertBack(data : number) : DoublePointedNode
-  {
-    const node = new DoublePointedNode(data);
-    if(!this.head) this.head = node;
-    else {
-      let current = this.head;
-      while(current.next !== null) { current = current.next; }
-      current.next = node;
-    }
-    ++this._count;
-    return node;
-  }
-  public traverse() : DoublePointedNode[] {
-    let nodes: DoublePointedNode[] = [];
-    let current = this.head;
-    while(current)
-    {
-      nodes.push(current);
-      current = current.next;
-    }
-    return nodes;
-  }
+  // public get pop() : DoublePointedNode | null
+  // {
+  //   if(!this.head) return null;
+  //   if(!this.head.next) return this.head;
+  //   let current = this.head;
+  //   let result = current 
+  // }
 }
--- app/src/app/shared/models/DoublePointedNode.ts
+++ app/src/app/shared/models/DoublePointedNode.ts
@@ -3,10 +3,12 @@ import * as uuid from 'uuid';
 export class DoublePointedNode {
   private _id : string;
   private _next : DoublePointedNode | null;
+  private _prev : DoublePointedNode | null;
   private _value : number;
   constructor(public init_value : number = 0)
   {
     this._id = uuid.v4();
+    this._prev = null;
     this._next = null;
     this._value = init_value;
   }
@@ -14,7 +16,9 @@ export class DoublePointedNode {
   public get id() : string { return this._id; };
   public get value() { return this._value; };
   public get next() : DoublePointedNode | null { return this._next; };
+  public get prev() : DoublePointedNode | null { return this._prev; };
   // Setters
   public set value(_value : number) { this._value = _value; };
   public set next(_next : DoublePointedNode | null) { this._next = _next; }
+  public set prev(_prev : DoublePointedNode | null) { this._prev = _prev; }
 }
